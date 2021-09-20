2615f3f5d4adcaa4f19ebf82d7b4564d49f84816
Bram Vandenbogaerde <bram@vdb.space>
2021-09-20 16:26:31 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# link DEST SOURCE
+function link() {
+	if [[ -e "$1" ]]; then 
+		echo "$1 already exists skipping..."
+	else 
+		echo "Destination config for $1 does not exist, installing..."
+		ln -s $2 $1
+	fi
+}
+
+link $HOME/.config/nvim $PWD/nvim

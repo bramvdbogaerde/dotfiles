c083fb7be1f4069f4c55eb9757ec45f41565dcd4
Bram Vandenbogaerde <bram@vdb.space>
2022-12-01 23:18:23 +0100

    lsp experiments

---
+++ nvim/lua/actor_lsp.lua
@@ -0,0 +1,20 @@
+local lspconfig = require("lspconfig")
+local configs = require 'lspconfig.configs'
+
+vim.cmd [[
+   autocmd BufNewFile,BufRead *.actor.scm setfiletype actor-scheme
+]]
+
+if not configs.actor_lsp then 
+   configs.actor_lsp = {
+      default_config = {
+         cmd = {"/datapool/phd-bench/maf-actors-lsp/run.sh"},
+         filetypes = {"actor-scheme"},
+         root_dir = lspconfig.util.root_pattern("actors-project"),
+         settings = {},
+         single_file_support = true
+      }
+   }
+end
+
+lspconfig.actor_lsp.setup{}

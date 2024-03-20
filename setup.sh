5c2ea074c422d444bf5a04822a82a44f75e85b63
Bram Vandenbogaerde <bram@vdb.space>
2024-03-20 11:22:11 +0100

    diff-so-fancy

--- setup.sh
+++ setup.sh
@@ -18,3 +18,5 @@ link $HOME/.config/hypr $PWD/hypr
 
 echo "setting up global gitignore"
 git config --global core.excludesFile "$PWD/gitignore"
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
